24085c6d88eaf0c9a076adf2c92017993fc9af89
Martin Elsässer <marels@aon.at>
2022-11-06 19:43:11 +0100

    Improved speed by lowering amount of operations needed. This is done using a bundeling aproach. Ram Registers which will be assigned the same value are all assigned the value one after the other, so the value only needs to be loaded once.

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const getPixels = require("get-pixels");
 const fs = require("fs");
 const util = require("util");
+const path = require("path");
 
 const asyncPixels = util.promisify(getPixels);
 
 !async function main() {
-	let pixels = await asyncPixels("./assembler.png");
+	let pixels = await asyncPixels(path.join(__dirname, "./docker.png"));
 	let pixelsInWhiteAndBlack = [];
 
 	for (let i = 0; i < pixels.data.length; i += 4) {
 		let blackWhiteVal = pixels.data[i] === 255 ? 0 : 1;
+		// let blackWhiteVal = pixels.data[i] === 255 ? 1 : 0;
 		pixelsInWhiteAndBlack.push(blackWhiteVal);
 	}
 	let pixelRegisterValues = get16bitPixelValues(pixelsInWhiteAndBlack);
-	let code = getCode(pixelRegisterValues);
-	fs.writeFileSync("output.asm", code);
-}()
+	let addressPixelValueMap = createPixelValueMap(pixelRegisterValues);
+	let code = getCode(addressPixelValueMap);
+	fs.writeFileSync("output.txt", code);
+}();
+
+function createPixelValueMap(pixelRegisterValues) {
+	let map = {};
+	let startingAddress = 16384;
+	pixelRegisterValues.forEach((registerVal, i) => {
+		if (registerVal === 0) return;
+		if (!map[registerVal]) map[registerVal] = [];
+		map[registerVal].push(startingAddress + i);
+	});
+	return map;
+}
 
 function get16bitPixelValues(pixels) {
 	let values = [];
@@ -23,20 +37,18 @@ function get16bitPixelValues(pixels) {
 		let sum = pixels
 			.slice(i, i + 16)
 			.map((el, i) => el * Math.pow(2, 15 - i))
-			.reduce((a, b) => a + b);
+			.reduce((a, b) => +a + +b);
 		values.push(sum);
 	}
 	return values;
 }
 
-function getCode(values) {
-	let startingAddress = 16384;
-	return values.map((value, i) => {
-		if (value === 0) return "";
-
-		let hex_addr = `0x${(i + startingAddress).toString(16)}`;
-		let splitNumbers = splitNumberIntoSmallerOnes(value);
-		let code = `# Set ${hex_addr} to ${value}\n`;
+function getCode(registerPixelMap) {
+	let code = "";
+	for (let pixelValue in registerPixelMap) {
+		// load pixel Value into D register
+		code += `# Load ${pixelValue} into D\n`;
+		let splitNumbers = splitNumberIntoSmallerOnes(parseInt(pixelValue));
 		splitNumbers.forEach((num, i) => {
 			// optimizations:
 			if (i >= 1 && num === splitNumbers[i - 1])
@@ -44,13 +56,21 @@ function getCode(values) {
 			if (i >= 1 && num === 1)
 				return code += "D=D+1\n";
 
-			code += `A=0x${num.toString(16)}\n`;
+			code += `A=0x${(num).toString(16)}\n`;
 			code += i === 0 ? "D=A\n" : "D=D+A\n";
 		});
-		code += `A=${hex_addr}\n` + `*A=D\n`;
-		return code;
-	}).join("");
+
+		// save value of D register into addresses
+		registerPixelMap[pixelValue].forEach(register => {
+			let hex_addr = `0x${register.toString(16)}`;
+			// code += `# Set ${hex_addr} to ${pixelValue}\n`;
+			code += `A=${hex_addr}\n` + `*A=D\n`;
+		})
+	}
+	return code;
 }
+
+
 
 function splitNumberIntoSmallerOnes(number) {
 	// break Numbers bigger than 0x7fff/32767
